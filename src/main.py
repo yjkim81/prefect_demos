0098fc6febcf86847214ae1fe80df18d82d4901a
yjkim81 <yjkim@ontune.co.kr>
2025-07-03 14:47:35 +0900

    add pytest workflow

---
+++ src/main.py
@@ -0,0 +1,6 @@
+def add(a: int, b: int) -> int:
+    return a + b
+
+
+if __name__ == "__main__":
+    print("2 + 3 =", add(2, 3))
---
+++ tests/test_main.py
@@ -0,0 +1,5 @@
+from src.main import add
+
+
+def test_add() -> None:
+    assert add(2, 3) == 5
